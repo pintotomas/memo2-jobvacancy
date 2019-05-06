8ab0cbe4eb5430b993d07ebaaafd59d51c930151
Tomas <tomasp834@gmail.com>
2019-05-06 14:02:28 -0300

    job application repository. Additional migrations. Test repository

--- models/job_application.rb
+++ models/job_application.rb
@@ -1,11 +1,14 @@
 class JobApplication
-  attr_accessor :applicant_email, :id, :job_offer_id
+  attr_accessor :applicant_email, :id, :job_offer_id,
+                :updated_on, :created_on
   include ActiveModel::Validations
 
   def initialize(data)
     @id = data[:id]
     @job_offer_id = data[:job_offer_id]
     @applicant_email = data[:email]
+    @updated_on = data[:updated_on]
+    @created_on = data[:created_on]
   end
 
   def process
