e68afa4529b0d577e5405fb787dd7f6615741b90
Tomas <tomasp834@gmail.com>
2019-05-06 04:06:29 -0300

    migration modifications

--- db/migrate/009_create_job_applications.rb
+++ db/migrate/009_create_job_applications.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   up do
     create_table(:job_applications) do
       primary_key :id
-      foreign_key :job_offer_id :job_offers, :on_delete :cascade
+      foreign_key :job_offer_id, :job_offers, on_delete: 'cascade'
       String :email
     end
   end
