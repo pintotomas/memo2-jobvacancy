2ca3f9a870fd0724995425d79cd0c71f54af388c
santiago bianco <santiago.bianco91@gmail.com>
2019-05-15 19:48:11 -0300

    fix para la experienncia cuando es cero

--- models/job_offer.rb
+++ models/job_offer.rb
@@ -6,7 +6,7 @@ class JobOffer
                 :validity_time, :satisfied, :experience
 
   validates :experience, numericality: { allow_nil: true, greater_than:
-    0, less_than_or_equal_to: 20 }
+    -1, less_than_or_equal_to: 20 }
   validates :title, presence: true
   validate :validate_date, :validate_time
 
