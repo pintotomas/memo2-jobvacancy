adbff4f9696a4c608a3f735d99e1c1790da54cbb
santiago bianco <santiago.bianco91@gmail.com>
2019-05-15 20:15:16 -0300

    cambio a un passwords validos los archivos donde se inicializan los usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ user_repository = UserRepository.new
 unless user_repository.all.count.positive?
   user = User.new(email: 'offerer@test.com',
                   name: 'Offerer',
-                  password: 'Passw0rd')
+                  password: 'Aa123456')
 
   user_repository.save user
 end
