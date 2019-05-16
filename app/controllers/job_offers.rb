0ed10b8e0296c32ec584a9a0c9d69b18729c28a2
Tomas <tomasp834@gmail.com>
2019-05-16 01:36:12 -0300

    delegate responsibility to model

--- app/controllers/job_offers.rb
+++ app/controllers/job_offers.rb
@@ -41,29 +41,24 @@ JobVacancy::App.controllers :job_offers do
 
   post :apply, with: :offer_id do
     @job_offer = JobOfferRepository.new.find(params[:offer_id])
-    if @job_offer.expired_offer? || @job_offer.old_offer?
-      flash[:error] = 'Offer expired while you were applying'
-      redirect '/job_offers/latest'
-    elsif @job_offer.satisfied?
-      flash[:error] = 'Offer was satisfied before you completed your application'
-      redirect '/job_offers/latest'
+    applicant_email = params[:job_application][:applicant_email]
+    bio = params[:job_application][:bio]
+    @job_application = JobApplication.new(email: applicant_email, offer: @job_offer,
+                                          job_offer_id: @job_offer.id, bio: bio)
+    if @job_application.valid?
+      JobApplicationRepository.new.save(@job_application)
+      @job_application.process(@job_offer)
+      flash[:success] = 'Contact information sent.'
     else
-      applicant_email = params[:job_application][:applicant_email]
-      bio = params[:job_application][:bio]
-      @job_application = JobApplication.new(email: applicant_email,
-                                            job_offer_id: @job_offer.id, bio: bio)
-      if @job_application.valid?
-        JobApplicationRepository.new.save(@job_application)
-        @job_application.process(@job_offer)
-        flash[:success] = 'Contact information sent.'
-        redirect '/job_offers'
+      @job_offer = JobOfferRepository.new.find(params[:offer_id])
+      flash[:error] = @job_application.errors.full_messages[0]
+      if @job_application.errors.details.key? :offer
+        redirect 'job_offers/latest'
       else
-        @job_offer = JobOfferRepository.new.find(params[:offer_id])
-        flash.now[:error] = @job_application.errors.full_messages[0]
-        render 'job_offers/apply'
+        redirect 'job_offers/apply/' + String(@job_offer.id)
       end
-
     end
+    redirect 'job_offers/latest' if @job_application.valid?
   end
 
   post :create do
