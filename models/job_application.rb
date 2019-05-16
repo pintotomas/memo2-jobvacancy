edff1bf9d3cee7b805d9c1f6849062ae7e86b26a
Tomas <tomasp834@gmail.com>
2019-05-15 23:34:32 -0300

    application should be valid when offer is not satisfied

--- models/job_application.rb
+++ models/job_application.rb
@@ -29,8 +29,9 @@ class JobApplication
 
   def validate_offer
     return if @job_offer.nil?
+    return unless @job_offer.satisfied?
 
     @not_valid = true
-    errors.add(:offer, 'invalid. It is already satisfied')
+    errors.add(:offer, 'was satisfied before you completed your application')
   end
 end
