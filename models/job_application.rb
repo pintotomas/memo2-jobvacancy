56f9fb6ceec429330a5aecc9e0b3e2b4ca8ffe1f
Tomas <tomasp834@gmail.com>
2019-05-15 23:26:28 -0300

    job application should not be valid if the offer was satisfied

--- models/job_application.rb
+++ models/job_application.rb
@@ -9,8 +9,9 @@ class JobApplication
                                                         message: ': invalid format' }
 
   validates :bio, presence: true, length: { minimum: 1, maximum: 500 }, allow_blank: false
-
+  validate :validate_offer
   def initialize(data)
+    @job_offer = data[:offer]
     @id = data[:id]
     @job_offer_id = data[:job_offer_id]
     @applicant_email = data[:email]
@@ -22,5 +23,14 @@ class JobApplication
   def process(offer)
     @job_offer = offer
     JobVacancy::App.deliver(:notification, :contact_info_email, self)
+  end
+
+  protected
+
+  def validate_offer
+    return if @job_offer.nil?
+
+    @not_valid = true
+    errors.add(:offer, 'invalid. It is already satisfied')
   end
 end
